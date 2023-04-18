3284a821cdfe6a08a696a7e4e608817784b648e9
trevormil <trevormil@comcast.net>
2023-04-18 15:26:29 -0400

    dynamic url

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { SupportedChain } from "bitbadges-sdk";
 
-export const HOSTNAME = process.env.BITBADGES_IO ? 'bitbadges.io' : 'localhost';
+export const HOSTNAME = window.location.hostname;
 
 export const NODE_URL = `http://${HOSTNAME}:1317`;
 export const BACKEND_URL = `https://${HOSTNAME}:3001`;
