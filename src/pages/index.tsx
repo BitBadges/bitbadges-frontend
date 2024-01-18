00aa1897f29760d0f1abf1416311ea6003935a72
Trevor Miller <trevormil@comcast.net>
2024-01-18 12:38:24 -0500

    fix notifications if same site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -448,6 +448,9 @@ const Home: NextPage = () => {
                       <br />
                       <SignInWithBitBadgesButton
                         onSignAndBlockinVerify={async () => {
+                          if (window && window.opener) {
+                            return
+                          }
                           notification.success({
                             message: 'Success',
                             description: `On a site that uses Sign In with BitBadges, you would now be signed in to the website with your selected address if you met the authentication and badge ownership requirements.`
